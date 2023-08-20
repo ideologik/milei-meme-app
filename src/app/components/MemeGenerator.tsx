df4b4a06e5da7f0232e90bf7a234a2e7c41a0dbc
Jorge Fox D'Angelo <jgdfox@gmail.com>
2023-08-20 12:16:06 -0300

    movile

--- src/app/components/MemeGenerator.tsx
+++ src/app/components/MemeGenerator.tsx
@@ -1,57 +1,70 @@
-'use client'
+"use client"
 
+// MemeGenerator.js
 import React, { useRef, useState, useEffect } from 'react';
 
 const MemeGenerator = () => {
     const [topText, setTopText] = useState('');
-    const [bottomText, setBottomText] = useState('');
+    const [bottomText, setBottomText] = useState('¡AFUERA!');
     const canvasRef = useRef(null);
-    const [imageWidth, setImageWidth] = useState(0);
+    const inputContainerRef: any = useRef(null);
+    const downloadLinkRef: any = useRef(null);
+    const imageRef = useRef(new Image());
+
+    useEffect(() => {
+        imageRef.current.src = './assets/milei_meme.jpg'; // Ruta de tu imagen
+        imageRef.current.onload = updateCanvas;
+    }, []);
 
     const updateCanvas = () => {
         const canvas: any = canvasRef.current;
         const ctx = canvas.getContext('2d');
-        const image = new Image();
-        image.src = './assets/milei_meme.jpg'; // Ruta de tu imagen
-        image.onload = () => {
-            const aspectRatio = image.width / image.height;
-            const newImageWidth = (window.innerHeight - 100) * aspectRatio;
-            setImageWidth(newImageWidth);
-            canvas.width = newImageWidth;
-            canvas.height = window.innerHeight - 100;
+        const maxHeight = window.innerHeight * 0.8;
+        const aspectRatio = imageRef.current.width / imageRef.current.height;
+        canvas.width = maxHeight * aspectRatio;
+        canvas.height = maxHeight;
 
-            ctx.drawImage(image, 0, 0, newImageWidth, canvas.height);
+        inputContainerRef.current.style.width = `${canvas.width}px`;
 
-            // Texto Superior
-            ctx.font = '20px Arial';
-            ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
-            ctx.fillRect(0, 10, canvas.width, 30);
-            ctx.fillStyle = 'white';
-            ctx.textAlign = 'center';
-            ctx.fillText(topText, canvas.width / 2, 30);
+        ctx.drawImage(imageRef.current, 0, 0, canvas.width, canvas.height);
 
-            // Texto Inferior
-            ctx.font = 'bold 30px Arial';
-            ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
-            ctx.fillRect(0, canvas.height - 50, canvas.width, 40);
-            ctx.fillStyle = 'white';
-            ctx.fillText(bottomText, canvas.width / 2, canvas.height - 20);
-        };
+        // Texto Superior
+        ctx.font = '20px Arial';
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+        ctx.fillRect(0, 10, canvas.width, 30);
+        ctx.fillStyle = 'white';
+        ctx.textAlign = 'center';
+        ctx.fillText(topText, canvas.width / 2, 30);
+
+        // Texto Inferior
+        ctx.font = 'bold 30px Arial';
+        ctx.fillStyle = 'rgba(0, 0, 0, 0.5)';
+        ctx.fillRect(0, canvas.height - 50, canvas.width, 40);
+        ctx.fillStyle = 'white';
+        ctx.fillText(bottomText, canvas.width / 2, canvas.height - 20);
+
+        if (downloadLinkRef.current) {
+            downloadLinkRef.current.href = canvas.toDataURL('image/jpg');
+            downloadLinkRef.current.download = 'meme.jpg';
+        }
     };
 
-    // Asegurarse de que el lienzo se actualice cuando se carga el componente
     useEffect(() => {
+        window.addEventListener('resize', updateCanvas);
         updateCanvas();
+        return () => window.removeEventListener('resize', updateCanvas);
     }, [topText, bottomText]);
 
     return (
         <div className="flex flex-col h-screen justify-center items-center bg-gray-200">
-            <div className="mb-4 flex space-x-4">
-                <input type="text" value={topText} onChange={e => { setTopText(e.target.value); updateCanvas(); }} className="p-2 border rounded" placeholder="Texto Superior" />
-                <input type="text" value={bottomText} onChange={e => { setBottomText(e.target.value); updateCanvas(); }} className="p-2 border rounded" placeholder="Texto Inferior" />
+            <div ref={inputContainerRef} className="mb-0 flex flex-col space-y-1">
+                <input type="text" value={topText} onChange={e => { setTopText(e.target.value); updateCanvas(); }} className="p-2 border rounded w-full" placeholder="Texto Superior" />
+                <input type="text" value={bottomText} onChange={e => { setBottomText(e.target.value); updateCanvas(); }} className="p-2 border rounded w-full" placeholder="Texto Inferior" />
             </div>
-            <div style={{ width: `${imageWidth}px` }}>
-                <canvas ref={canvasRef} />
+            <div>
+                <a ref={downloadLinkRef}>
+                    <canvas ref={canvasRef} />
+                </a>
             </div>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@ import MemeGenerator from "./components/MemeGenerator";
 
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen justify-center items-center bg-gray-200">
-      <h1>Generador de memes de Milei by fox</h1>
-      <MemeGenerator />
+    <div className="flex flex-col min-h-screen items-center bg-gray-200">
+      <div className="mt-0 mb-0 text-center">
+        <h1 className="text-2xl font-bold">Generador de memes de Milei by fox</h1>
+      </div>
+      <div className="flex-1 flex flex-col justify-center items-center">
+        <MemeGenerator />
+      </div>
     </div>
   )
 }
